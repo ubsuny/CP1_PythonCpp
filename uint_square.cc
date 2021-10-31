1bf2d89b0fb6a7015d9ac2be1bdd66d337436f71
piaohanx <36085924+piaohanx@users.noreply.github.com>
2021-10-31 13:29:23 -0400

    C++ codes for generating the square series
    uint_square.cc uses the unsigned integer data type to store the terms in the series.
    float_square.cc uses the float data type to store the terms in the series.
    double_square.cc uses the double data type to store the terms in the series.

---
+++ uint_square.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; 
+    unsigned int t1 = 2, nextTerm = 0;
+
+    cout << "Enter the largest value of n: ";
+    cin >> n;
+
+    cout << "Square Series: ";
+
+    for (int i = 0; i <= n; ++i) {
+        // Prints the first term.
+        if(i == 0) {
+            cout << t1 << ", ";
+            continue;
+        }
+        
+        nextTerm = t1 * t1;
+        
+        t1 = nextTerm;
+        
+        cout << nextTerm << ", ";
+    }
+    return 0;
+}
